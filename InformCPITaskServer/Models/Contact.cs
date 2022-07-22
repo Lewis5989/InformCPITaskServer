64e4c7e0f6f43a6570dabca3bb4da97f962b8623
Lewis5989 <lewis_ac@hotmail.co.uk>
2022-07-22 13:15:01 +0100

    Added Database models for User and Contact

---
+++ InformCPITaskServer/Models/Contact.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace InformCPITaskServer.Models
+{
+    public class Contact
+    {
+        public int Id { get; set; }
+        [Required]
+        public int UserId { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? ContactName{ get; set; }
+    }
+}

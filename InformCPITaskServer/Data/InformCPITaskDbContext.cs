1729d467681be1cb8cea85c00082afe20218e75c
Lewis5989 <lewis_ac@hotmail.co.uk>
2022-07-22 13:35:59 +0100

    Added DbContext service for informCPITask database and registered it with the builder in program.cs

---
+++ InformCPITaskServer/Data/InformCPITaskDbContext.cs
@@ -0,0 +1,15 @@
+﻿using InformCPITaskServer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InformCPITaskServer.Data
+{
+    public class InformCPITaskDbContext:DbContext
+    {
+        public InformCPITaskDbContext(DbContextOptions<InformCPITaskDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Contact> Contacts { set; get; }
+        public DbSet<User> Users { get; set; }
+    }
+}

ca2305680ac69bceb127d0480e0d1f85167c9dd1
Lewis5989 <lewis_ac@hotmail.co.uk>
2022-07-25 01:12:13 +0100

    corrected db scripts

--- DatabaseScripts/DummyDataScript.sql
+++ DatabaseScripts/DummyDataScript.sql
@@ -1,14 +1,14 @@
 
-INSERT INTO Users(FirstName,LastName, EMail, Password)
-VALUES ('John', 'Ironside', 'johns@email.com','Password'),
-('Hannah', 'Ironside', 'Hannah@email.com','Password'),
-('John', 'Smith', 'john@email.com','Password'),
-('Ian', 'Hancock', 'Ians@email.com','Password'),
-('Nick', 'Patel', 'Nicks@email.cm','Password');
+INSERT INTO Users(UserName, EMail, Password)
+VALUES ('John Ironside', 'johns@email.com','Password'),
+('Hannah Ironside', 'Hannah@email.com','Password'),
+('John Smith', 'john@email.com','Password'),
+('Ian Hancock', 'Ians@email.com','Password'),
+('Nic Patel', 'Nicks@email.cm','Password');
 
-INSERT INTO Contacts(UserID,FirstName,LastName, EMail, PhoneNumber)
-VALUES (1,null, null, null,'07708756565'),
-(1,'Hannah', 'Ironside', 'Hannah@email.com','07708756501'),
-(1,'John', 'Smith', 'john@email.com','07708756502'),
-(1,'Ian', 'Hancock', null,'07708756503'),
-(1,'Nick', null, 'Nicks@email.com',null);
+INSERT INTO Contacts(UserID,ContactName, EMail, PhoneNumber)
+VALUES ( null, null,'07708756565'),
+('Hannah Ironside', 'Hannah@email.com','07708756501'),
+('John Smith', 'john@email.com','07708756502'),
+('Ian Hancock', null,'07708756503'),
+('Nick','Nicks@email.com',null);
